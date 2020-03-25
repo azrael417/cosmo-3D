fed1457824a3faf61fc5fb83a682bc9041fe4dd6
vmilovanovic <vmilovanovic@nvidia.com>
2020-03-25 16:03:30 -0700

    initial commit

---
+++ docker/build_docker.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+#nvidia-docker build -t tkurth/pytorch-bias_gan:latest .
+cd ..
+nvidia-docker build -t registry.services.nersc.gov/tkurth/pytorch-cosmo:latest -f docker/Dockerfile .
+#docker push registry.services.nersc.gov/tkurth/pytorch-bias_gan:latest
+
+#run docker test
+#docker run --device=/dev/nvidia-fs0 --workdir "/opt/pytorch/numpy_reader/scripts" -it tkurth/pytorch-numpy_reader:latest ./reader_test.sh
+#nvidia-docker run --workdir "/opt/pytorch/numpy_reader/scripts" -it tkurth/pytorch-numpy_reader:latest ./reader_test.sh
---
+++ docker/run_docker_gds_raid4.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+#some parameters
+#data_root=/home/cuda/tkurth/cam5_data
+
+#inference runs
+nvidia-docker run \
+	      --ipc host \
+              --device /dev/nvidia-fs0  \
+	      --device /dev/nvidia-fs1  \
+	      --device /dev/nvidia-fs2  \
+	      --device /dev/nvidia-fs3  \
+	      --device /dev/nvidia-fs4  \
+	      --device /dev/nvidia-fs5  \
+	      --device /dev/nvidia-fs6  \
+	      --device /dev/nvidia-fs7  \
+	      --device /dev/nvidia-fs8  \
+	      --device /dev/nvidia-fs9  \
+	      --device /dev/nvidia-fs10 \
+	      --device /dev/nvidia-fs11 \
+	      --device /dev/nvidia-fs12 \
+	      --device /dev/nvidia-fs13 \
+	      --device /dev/nvidia-fs14 \
+	      --device /dev/nvidia-fs15 \
+	      --volume "/raid1/data/tkurth:/data1:rw" \
+	      --volume "/raid3/data/tkurth:/data3:rw" \
+	      --volume "/raid5/data/tkurth:/data5:rw" \
+	      --volume "/raid7/data/tkurth:/data7:rw" \
+	      --workdir "/opt/DDP_UNet" -it registry.services.nersc.gov/tkurth/pytorch-cosmo:latest /bin/bash
