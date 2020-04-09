85536ec24abc123378f583b2f61175033a9dd7a8
Thorsten Kurth <thorstenkurth@me.com>
2020-04-09 11:21:39 -0700

    tailoring script to cori

--- src/DDP_UNet/data_bench_dali_lowmem.py
+++ src/DDP_UNet/data_bench_dali_lowmem.py
@@ -19,9 +19,11 @@ device = torch.device("cuda:{}".format(comm_local_rank))
 stage = True
 if stage:
     # copy the input file into local DRAM for each socket:
+    #tmpfs_root = '/dev/shm'
+    tmpfs_root = '/run/cosmo_data'
     gpus_per_socket = torch.cuda.device_count() // 2
     socket = 0 if comm_rank < gpus_per_socket else 1
-    new_data_path = os.path.join('/dev/shm', 'socket_{}'.format(socket), os.path.basename(params.data_path))
+    new_data_path = os.path.join(tmpfs_root, 'socket_{}'.format(socket), os.path.basename(params.data_path))
     if comm_rank % (torch.cuda.device_count() / 2) == 0:
         if not os.path.isdir(os.path.dirname(new_data_path)):
             os.makedirs(os.path.dirname(new_data_path))
