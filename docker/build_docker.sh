60dc20417ab9bd7b717f3ccf4186a939f33873dd
vmilovanovic <vmilovanovic@nvidia.com>
2020-04-09 11:18:50 -0700

    fixing some issues

--- docker/build_docker.sh
+++ docker/build_docker.sh
@@ -3,7 +3,7 @@
 #nvidia-docker build -t tkurth/pytorch-bias_gan:latest .
 cd ..
 nvidia-docker build -t registry.services.nersc.gov/tkurth/pytorch-cosmo:latest -f docker/Dockerfile .
-#docker push registry.services.nersc.gov/tkurth/pytorch-bias_gan:latest
+docker push registry.services.nersc.gov/tkurth/pytorch-cosmo:latest
 
 #run docker test
 #docker run --device=/dev/nvidia-fs0 --workdir "/opt/pytorch/numpy_reader/scripts" -it tkurth/pytorch-numpy_reader:latest ./reader_test.sh
