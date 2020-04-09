6de59903a3ad3bc6c3d7e60e9f0622736562db80
vmilovanovic <vmilovanovic@nvidia.com>
2020-04-09 10:32:20 -0700

    added data staging to lowmem script

--- src/DDP_UNet/data_bench_dali.py
+++ src/DDP_UNet/data_bench_dali.py
@@ -2,13 +2,19 @@ from utils.YParams import YParams
 import torch
 from utils.data_loader_dali import get_data_loader_distributed
 import time
+import torch.distributed as dist
+
+#init process group
+dist.init_process_group(backend = "mpi")
+comm_rank = dist.get_rank()
+comm_local_rank = comm_rank % torch.cuda.device_count()
 
 #load parameters
 params = YParams("config/UNet_transpose.yaml", "default")
-device = torch.device("cuda:0")
+device = torch.device("cuda:{}".format(comm_local_rank))
 
 # get data loader
-train_data_loader = get_data_loader_distributed(params, 0)
+train_data_loader = get_data_loader_distributed(params, comm_rank, comm_local_rank)
 
 it = 0
 tstart = time.time()
--- src/DDP_UNet/data_bench_dali_lowmem.py
+++ src/DDP_UNet/data_bench_dali_lowmem.py
@@ -1,18 +1,47 @@
 from utils.YParams import YParams
+import os
+import shutil as shu
 import torch
 from utils.data_loader_dali_lowmem import get_data_loader_distributed
 import time
+import torch.distributed as dist
 
-#load parameters
+# init process group
+dist.init_process_group(backend = "mpi")
+comm_rank = dist.get_rank()
+comm_local_rank = comm_rank % torch.cuda.device_count()
+
+# load parameters
 params = YParams("config/UNet_transpose.yaml", "default")
-device = torch.device("cuda:0")
+device = torch.device("cuda:{}".format(comm_local_rank))
+
+# stage in?
+stage = True
+if stage:
+    # copy the input file into local DRAM for each socket:
+    gpus_per_socket = torch.cuda.device_count() // 2
+    socket = 0 if comm_rank < gpus_per_socket else 1
+    new_data_path = os.path.join('/dev/shm', 'socket_{}'.format(socket), os.path.basename(params.data_path))
+    if comm_rank % (torch.cuda.device_count() / 2) == 0:
+        if not os.path.isdir(os.path.dirname(new_data_path)):
+            os.makedirs(os.path.dirname(new_data_path))
+        shu.copyfile(params.data_path, new_data_path)
+
+    # we need to wait till the stuff is copied
+    dist.barrier()
+    
+    # change parameter path to new file
+    params.data_path = new_data_path
 
 # get data loader
-train_data_loader = get_data_loader_distributed(params, 0)
+train_data_loader = get_data_loader_distributed(params, comm_rank, comm_local_rank)
 
 it = 0
+# sync here so that we can start a timer
+dist.barrier()
 tstart = time.time()
 for inp, tar in train_data_loader:
     it += 1
+dist.barrier()
 tend = time.time()
 print("Iterations took {}s for {} iterations ({} iter/s)".format(tend - tstart, it, float(it)/(tend - tstart)))
