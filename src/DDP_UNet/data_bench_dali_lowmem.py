80081d841f980d8594c82bb0d5343d238e29c513
vmilovanovic <vmilovanovic@nvidia.com>
2020-04-09 12:16:48 -0700

    using shm

--- src/DDP_UNet/data_bench_dali_lowmem.py
+++ src/DDP_UNet/data_bench_dali_lowmem.py
@@ -23,8 +23,8 @@ device = torch.device("cuda:{}".format(comm_local_rank))
 # stage in?
 if stage:
     # copy the input file into local DRAM for each socket:
-    #tmpfs_root = '/dev/shm'
-    tmpfs_root = '/run/cosmo_data'
+    tmpfs_root = '/dev/shm'
+    #tmpfs_root = '/run/cosmo_data'
     gpus_per_socket = torch.cuda.device_count() // 2
     socket = 0 if comm_rank < gpus_per_socket else 1
     new_data_path = os.path.join(tmpfs_root, 'socket_{}'.format(socket), os.path.basename(params.data_path))
