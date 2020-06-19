edaad21f255e5ab28c523622d50e93e886f9e05f
Awesom E. Dev <devnull@nvidia.com>
2020-06-18 17:37:25 -0700

    fixing dockerfiles

--- docker/build_docker.sh
+++ docker/build_docker.sh
@@ -8,17 +8,24 @@ cd ..
 #	      --build-arg PYVER=3.7 \
 #	      --build-arg PYV=37 \
 #	      -f docker/Dockerfile.deps.ubuntu .
+#exit
 
-# pytorch
-#nvidia-docker build -t registry.services.nersc.gov/tkurth/pytorch-cosmo.pytorch -f docker/Dockerfile.pytorch .
+## pytorch
+#nvidia-docker build -t registry.services.nersc.gov/tkurth/pytorch-cosmo.pytorch \
+#	      --build-arg PYVER=3.7 \
+#	      --build-arg PYV=37 \
+#	      -f docker/Dockerfile.pytorch .
 #exit
 
 # dali
-#nvidia-docker build -t registry.services.nersc.gov/tkurth/pytorch-cosmo.dali -f docker/Dockerfile.dali .
+nvidia-docker build -t registry.services.nersc.gov/tkurth/pytorch-cosmo.dali \
+	      --build-arg PYVER=3.7 \
+	      --build-arg PYV=37 \
+	      -f docker/Dockerfile.dali .
 
 # cosmo 3d
 nvidia-docker build -t registry.services.nersc.gov/tkurth/pytorch-cosmo:latest -f docker/Dockerfile .
-#docker push registry.services.nersc.gov/tkurth/pytorch-cosmo:latest
+docker push registry.services.nersc.gov/tkurth/pytorch-cosmo:latest
 
 #run docker test
 #docker run --device=/dev/nvidia-fs0 --workdir "/opt/pytorch/numpy_reader/scripts" -it tkurth/pytorch-numpy_reader:latest ./reader_test.sh
