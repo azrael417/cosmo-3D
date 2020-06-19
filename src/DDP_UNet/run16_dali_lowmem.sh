ee77672c8fe7a911c57fbe924a4537290ce0c183
Thorsten Kurth <tkurth@nvidia.com>
2020-06-19 18:05:24 +0000

    added smooth training

---
+++ src/DDP_UNet/run16_dali_lowmem.sh
@@ -0,0 +1,21 @@
+#!/bin/bash -l
+
+#number of ranks
+totalranks=16
+
+#hdf file locking 
+export HDF5_USE_FILE_LOCKING=FALSE
+
+#mpi stuff
+mpioptions="--allow-run-as-root --map-by ppr:8:socket:PE=3"
+
+# profile
+#profilecmd="nsys profile --stats=true --mpi-impl=openmpi --trace=cuda,cublas,nvtx,osrt,mpi -f true -o /data/profiles/dali_pipe/profile_dali_amp_threads_nranks${totalranks}_%q{OMPI_COMM_WORLD_RANK}"
+profilecmd=
+
+#srun
+mpirun -np ${totalranks} ${mpioptions} \
+     ${profilecmd} $(which python) train_dali_lowmem.py \
+     --yaml_config "config/UNet_transpose.yaml" \
+     --comm_mode "openmpi-nccl" \
+     --enable_amp
