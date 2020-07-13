f24b6489e27929641a16e9e431b6e414492cd0b7
Thorsten Kurth <tkurth@nvidia.com>
2020-07-13 22:47:17 +0000

    added no copy option to ES

--- src/DDP_UNet/run16_dali_nocopy.sh
+++ src/DDP_UNet/run16_dali_nocopy.sh
@@ -10,7 +10,7 @@ mpioptions="--allow-run-as-root --map-by ppr:8:socket:PE=3"
 for totalranks in 16; do
 
     # tag
-    tag="dali_amp_threads_nranks${totalranks}"
+    tag="dali-nocopy_amp_threads_nranks${totalranks}"
 
     # files
     outfile="/data/profiles/timing_${tag}.out"
@@ -20,6 +20,6 @@ for totalranks in 16; do
     mpirun -np ${totalranks} ${mpioptions} \
 	   ${profilecmd} $(which python) train_dali.py \
 	   --yaml_config "config/UNet_transpose.yaml" \
-	   --comm_mode "openmpi-nccl" --enable_amp |& tee ${outfile}
+	   --comm_mode "openmpi-nccl" --enable_amp --no_copy |& tee ${outfile}
 
 done
