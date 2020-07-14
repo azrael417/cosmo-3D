ab264e6df9d1211920b935ce3090af15b831d237
Thorsten Kurth <tkurth@nvidia.com>
2020-07-14 00:01:03 +0000

    fixing issues

--- src/DDP_UNet/run16_dali_lowmem.sh
+++ src/DDP_UNet/run16_dali_lowmem.sh
@@ -10,11 +10,12 @@ export HDF5_USE_FILE_LOCKING=FALSE
 mpioptions="--allow-run-as-root --map-by ppr:8:socket:PE=3"
 
 # profile
-profilecmd="nsys profile --stats=true --mpi-impl=openmpi --trace=cuda,cublas,nvtx,osrt,mpi -f true -o /data/profiles/dali_pipe/profile_dali_lowmem_amp_threads_nranks${totalranks}_%q{OMPI_COMM_WORLD_RANK}"
+#profilecmd="nsys profile --stats=true --mpi-impl=openmpi --trace=cuda,cublas,nvtx,osrt,mpi -f true -o /data/profiles/dali_pipe/profile_dali_lowmem_amp_threads_nranks${totalranks}_%q{OMPI_COMM_WORLD_RANK}"
 
 #srun
 mpirun -np ${totalranks} ${mpioptions} \
      ${profilecmd} $(which python) train_dali_lowmem.py \
      --yaml_config "config/UNet_transpose.yaml" \
      --comm_mode "openmpi-nccl" \
+     --no_copy \
      --enable_amp
